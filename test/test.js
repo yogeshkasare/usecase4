6a3cf2662ba75eccb8c0c11843f12bceb6b8cab3
yogeshkasare <yogesh.kasare@ltimindtree.com>
2025-01-22 13:30:19 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../index');
+const server = require('../index');  // Import the server instance
 const should = chai.should();
 
 chai.use(chaiHttp);
@@ -14,5 +14,10 @@ describe('GET /', () => {
         res.text.should.be.eql('Hello World!');
         done();
       });
+  });
+
+  // Close the server after tests
+  after(() => {
+    server.close();
   });
 });
